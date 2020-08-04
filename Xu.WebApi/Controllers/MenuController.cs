d44ad2388509ee25d0580aa764a07312952a13fc
xudahe <2458575640@qq.com>
2020-08-04 19:32:57 +0800

    2

--- Xu.WebApi/Controllers/MenuController.cs
+++ Xu.WebApi/Controllers/MenuController.cs
@@ -27,7 +27,7 @@ namespace Xu.WebApi.Controllers
         }
 
         /// <summary>
-        /// 获取全部菜单
+        /// 获取全部菜单（列表）
         /// </summary>
         /// <param name="menuName">菜单名称（可空）</param>
         /// <returns></returns>
@@ -66,7 +66,7 @@ namespace Xu.WebApi.Controllers
         }
 
         /// <summary>
-        /// 根据菜单Ids集合获取菜单数据
+        /// 根据菜单Ids集合获取菜单数据（树状）
         /// </summary>
         /// <param name="ids">可空</param>
         /// <returns></returns>
