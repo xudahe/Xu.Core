21e3be9d2faeca6ff3bd43d2098e42eea5698e8a
xudahe <2458575640@qq.com>
2021-01-07 17:35:03 +0800

    优化

---
+++ Xu.Model/Models/Notes/FileDirectory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xu.Model.Models
+{
+    /// <summary>
+    /// 文件夹目录
+    /// </summary>
+    public class FileDirectory : ModelBase
+    {
+        /// <summary>
+        /// 当前文件名显示等级
+        /// </summary>
+        public int Level { get; set; }
+
+        /// <summary>
+        /// 当前文件父级文件夹Id
+        /// </summary>
+        public int? ParentId { get; set; }
+
+        /// <summary>
+        /// 当前文件父级文件夹名称
+        /// </summary>
+        public string ParentName { get; set; }
+
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 当前文件是否为文件夹
+        /// </summary>
+        public int IsDirectory { get; set; }
+
+        /// <summary>
+        /// 实体路径
+        /// </summary>
+        public string EntityPath { get; set; }
+    }
+}
