1307e534eed4e2d5fccf2cbeb688e82020625819
xudahe <2458575640@qq.com>
2022-03-11 14:26:36 +0800

    1

--- Xu.Publish.bat
+++ Xu.Publish.bat
@@ -8,11 +8,11 @@ dotnet build
 
 cd Xu.WebApi
 
-dotnet publish -o ..\Xu.WebApi\bin\Debug\netcoreapp3.1\
+dotnet publish -o ..\Xu.WebApi\bin\Debug\net5.0\
 
 md ..\.publishFiles
 
-xcopy ..\Xu.WebApi\bin\Debug\netcoreapp3.1\*.* ..\.publishFiles\ /s /e 
+xcopy ..\Xu.WebApi\bin\Debug\net5.0\*.* ..\.publishFiles\ /s /e 
 
 echo "Successfully!!!! ^ please see the file .publishFiles"
 
